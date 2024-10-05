d18a041cfd0f694880a7a5e6ea742aaeec07bb28
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-05 14:10:12 -0400

    update: workflow setup matrix

--- .github/utils/setup-matrix.js
+++
@@ -1,9 +0,0 @@
-const MAX_RUNNERS = process.argv[2];
-const runners = [];
-
-if (MAX_RUNNERS <= 0) throw new Error('Invalid number of runners');
-for (let index = 1; index <= MAX_RUNNERS; index++) {
-    runners.push(index);
-}
-
-process.stdout.write(JSON.stringify(runners));
