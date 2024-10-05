21baca74e1227dca00b39d0031a6b1ef90df64e6
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-10-05 13:50:42 -0400

    add: github utils to create matrix

---
+++ .github/utils/setup-matrix.js
@@ -0,0 +1,9 @@
+const MAX_RUNNERS = process.argv[2];
+const runners = [];
+
+if (MAX_RUNNERS <= 0) throw new Error('Invalid number of runners');
+for (let index = 1; index <= MAX_RUNNERS; index++) {
+    runners.push(index);
+}
+
+process.stdout.write(runners.join(','));
